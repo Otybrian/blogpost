7d8af6f713fe1acc9d7c905194711e66cfd0fb02
Otybrian <brian.otieno@student.moringaschool.com>
2022-03-13 07:53:38 +0300

    create table subscribers that will save and send mails to the subscribers

--- app/models.py
+++ app/models.py
@@ -93,3 +93,17 @@ class Blog(db.Model):
 
     def __repr__(self):
         return f'Blog {self.title}'
+
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
