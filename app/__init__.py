a9609966ba4ad5eda9d0b9ee822133257d843461
Otybrian <brian.otieno@student.moringaschool.com>
2022-03-13 08:16:33 +0300

    register the authentication blueprint on app init file

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,11 @@ def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
+    from .auth import auth as authentication_blueprint
+    from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
 
     login_manager.init_app(app)
     db.init_app(app)
